d7b7ca86f97e90ff4abee33af9a6214026ec3e8d
ahmmohs <awsafsikdar@gmail.com>
2019-09-28 23:10:20 -0400

    navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+import '../styles/styles.css';
+
+import Navbar from './Navbar';
+
+class App extends React.Component {
+	render() {
+    	return (
+      		<BrowserRouter>
+        		<div className="container">
+					<Navbar />
+        		</div>
+      		</BrowserRouter>
+    	);
+  	}
 }
 
 export default App;
+
---
+++ src/components/Discover.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Discover extends React.Component {
+
+	render() {
+		return (
+			<div className="discover__wrapper">
+				
+			</div>
+		)
+	}
+
+}
